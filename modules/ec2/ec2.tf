376078f6f53b8f8e5894095e9768c59c60bd907a
Andres Bolivar <andresbolivar19@gmail.com>
2022-10-14 16:17:29 -0500

    Add security group to EC2

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -9,6 +9,13 @@ resource "aws_security_group" "security_group_ec2" {
   ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -24,12 +31,16 @@ resource "aws_security_group" "security_group_ec2" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "tcp"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags= {
     Name = var.security_group
+  }
+
+  lifecycle {
+  create_before_destroy = true
   }
 }
 
@@ -38,6 +49,7 @@ resource "aws_instance" "ec2" {
   instance_type           = var.instance_type
   key_name                = var.key_name
   vpc_security_group_ids  = [aws_security_group.security_group_ec2.id]
+  #private_ip              = "172.31.46.179"
 
   tags = {
     Name = var.name_ec2
