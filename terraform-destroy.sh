9962d019a307160302a8862761067a5932ef1fd2
Andres Bolivar <andresbolivar19@gmail.com>
2022-10-14 17:00:03 -0500

    Add workflow destroy

--- terraform-destroy.sh
+++ terraform-destroy.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -x
 
+#terraform -chdir=environments/$ENV_NAME init -reconfigure -input=false
+
+#terraform -chdir=environments/$ENV_NAME plan -destroy -input=false -var-file="../../variables/$ENV_NAME.tfvars"
+
 #terraform -chdir=environments/$ENV_NAME destroy -auto-approve -input=false -var-file="../../variables/$ENV_NAME.tfvars"
+
+terraform -chdir=modules/ec2 init -reconfigure -input=false
+
+terraform -chdir=modules/ec2 plan -destroy -input=false -var-file="../../variables/$ENV_NAME.tfvars"
 
 terraform -chdir=modules/ec2 destroy -auto-approve -input=false -var-file="../../variables/$ENV_NAME.tfvars"
