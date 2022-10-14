2a1654fa70a8b099aba8c02e8129178573ffc3ce
Andres Bolivar <andresbolivar19@gmail.com>
2022-10-13 22:56:09 -0500

    Terraform init Ok, plan error

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "ec2" {
   ami               = var.ami
   #ami               = "ami-08d4ac5b634553e16"
   #availability_zone = var.availability_zone
-  tags              = local.tags
+  #tags              = local.tags
   instance_type     = var.instance_type
   #instance_type     = "t2.micro"
 }
