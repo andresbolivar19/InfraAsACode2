71b692ed85a7bf8cea021c4b88cea64c72800bfb
Andres Bolivar <andresbolivar19@gmail.com>
2022-10-13 23:48:17 -0500

    Terraform apply

--- terraform-init-plan.sh
+++ terraform-init-plan.sh
@@ -5,8 +5,10 @@ set -x
 
 #terraform -chdir=environments/$ENV_NAME plan -input=false -var-file="../../variables/$ENV_NAME.tfvars"
 
+#terraform -chdir=environments/$ENV_NAME apply -auto-approve -input=false -var database_password=$TF_VAR_database_password -var-file="../../variables/$ENV_NAME.tfvars"
+
 terraform -chdir=modules/ec2 init -reconfigure -input=false
 
 terraform -chdir=modules/ec2 plan -input=false -var-file="../../variables/$ENV_NAME.tfvars"
 
-#terraform -chdir=environments/$ENV_NAME apply -auto-approve -input=false -var database_password=$TF_VAR_database_password -var-file="../../variables/$ENV_NAME.tfvars"
+terraform -chdir=modules/ec2 apply -auto-approve -input=false -var database_password=$TF_VAR_database_password -var-file="../../variables/$ENV_NAME.tfvars"
