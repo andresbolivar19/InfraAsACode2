1dd208d0a8a5412e13d19f489979a7fedb2b113c
Andres Bolivar <andresbolivar19@gmail.com>
2022-10-13 22:53:49 -0500

    Terraform init Ok, plan error

--- terraform-init-plan.sh
+++ terraform-init-plan.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 set -x
 
-terraform -chdir=environments/$ENV_NAME init -reconfigure -input=false
+#terraform -chdir=environments/$ENV_NAME init -reconfigure -input=false
 
-terraform -chdir=environments/$ENV_NAME plan -input=false -var-file="../../variables/$ENV_NAME.tfvars"
+#terraform -chdir=environments/$ENV_NAME plan -input=false -var-file="../../variables/$ENV_NAME.tfvars"
+
+terraform -chdir=modules/ec2 init -reconfigure -input=false
+
+terraform -chdir=modules/ec2 plan -input=false -var-file="../../variables/$ENV_NAME.tfvars"
 
 #terraform -chdir=environments/$ENV_NAME apply -auto-approve -input=false -var database_password=$TF_VAR_database_password -var-file="../../variables/$ENV_NAME.tfvars"
