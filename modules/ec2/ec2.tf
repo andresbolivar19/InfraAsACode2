878c0c2db7713fc26eb3b4082fa261047698f77b
Andres Bolivar <andresbolivar19@gmail.com>
2022-10-13 22:36:57 -0500

    Terraform init Ok, plan error

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -1,11 +1,13 @@
 provider "aws" {
-  #region = "us-east-2"
-  region  = var.aws_region
+  region = "us-east-2"
+  #region  = var.aws_region
 }
 
 resource "aws_instance" "ec2" {
-  ami               = var.ami
+  #ami               = var.ami
+  ami               = "ami-08d4ac5b634553e16"
   #availability_zone = var.availability_zone
   tags              = local.tags
-  instance_type     = var.instance_type
+  #instance_type     = var.instance_type
+  instance_type     = "t2.micro"
 }
--- variables/develop.tfvars
+++ variables/develop.tfvars
@@ -1,11 +1,7 @@
-# Common
-project = "InfraAsACode"
-
 # General 
 aws_region = "us-east-2"
 # Us East (Ohio)
 region = "us-east-2"
-createdBy  = "AFBN"
 
 ami="ami-08d4ac5b634553e16"
 instance_type = "t2.micro"
